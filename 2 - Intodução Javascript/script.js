c09a47a4f9607bde7b306454133f90682568e053
Danilo Gonçalves <95943821+gonclv@users.noreply.github.com>
2022-07-20 12:06:37 -0300

    Create script.js

---
+++ 2 - Intodução Javascript/script.js
@@ -0,0 +1,10 @@
+alert("Teste")
+
+//Para abrir o console: F12
+console.log ("Exemplo de log de texto.");
+
+let nomeDaVariavel = 10;
+console.log(nomeDaVariavel);
+
+let nomeDoTecnico = "Danilo";
+console.log (nomeDoTecnico);
