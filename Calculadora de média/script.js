66e2a6d0c91a4ab254f97a9c603da1adda2ecbe1
Danilo Gonçalves <95943821+gonclv@users.noreply.github.com>
2023-07-09 21:48:38 -0300

    Novos projetos - calc. média e portfólio

---
+++ Calculadora de média/script.js
@@ -0,0 +1,14 @@
+var nome = "Danilo"
+
+console.log('Bem vindo ' + nome)
+
+var notaDoPrimeiroBimestre = 10.0
+var notaDoSegundoBimestre = 9.554
+var notaDoTerceiroBimestre = 8.669
+var notaDoQuartoBimestre = 6.420
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFinal = notaFinal.toFixed(1)
+
+console.log(notaFinal)
